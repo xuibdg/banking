51e3f731ae2deeee091612db4371ba2aebcc9727
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-05-28 16:10:27 +0700

    fixing code

--- src/main/java/com/core/banking/entity/DepositAccount.java
+++ src/main/java/com/core/banking/entity/DepositAccount.java
@@ -1,7 +1,17 @@
 package com.core.banking.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
