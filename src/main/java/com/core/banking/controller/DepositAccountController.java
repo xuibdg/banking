8adf13abe0fd12649a90497f9afa0800196c9e61
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-06-01 01:53:39 +0700

    minor edit fix from comment

--- src/main/java/com/core/banking/controller/DepositAccountController.java
+++ src/main/java/com/core/banking/controller/DepositAccountController.java
@@ -9,7 +9,13 @@ import com.core.banking.enums.DepositAccountStatus;
 import com.core.banking.service.DepositAccountService;
 import com.core.banking.utils.exception.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.List;
 
