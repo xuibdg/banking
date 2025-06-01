0edd736a2201bcc32b2e81e5bf6afaacd0e2faf9
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-06-01 19:13:56 +0700

    final push merge into develop

--- src/main/java/com/core/banking/entity/DepositAccount.java
+++ src/main/java/com/core/banking/entity/DepositAccount.java
@@ -2,7 +2,18 @@ package com.core.banking.entity;
 
 import com.core.banking.enums.DepositAccountStatus;
 import com.core.banking.enums.RolloverOption;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -58,9 +69,6 @@ public class DepositAccount {
 
     @Column(name = "created_by")
     private String createdBy;
-
-//    @Column(name = "is_deleted")
-//    private boolean isDeleted = false;
 
     @Column(name = "opened_at")
     private LocalDateTime openedAt = LocalDateTime.now();
