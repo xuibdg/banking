c19bd887057e979ec774345f89073aa8851a3b38
rio <ainktz3089@gmail.com>
2025-06-22 23:57:59 +0700

    fixing Saving Account

--- src/main/java/com/core/banking/controller/SavingAccountController.java
+++ src/main/java/com/core/banking/controller/SavingAccountController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 import static com.core.banking.controller.BaseCRUDController.buildSuccessResponse;
 
 @RestController
--- src/main/java/com/core/banking/repository/SavingAccountRepository.java
+++ src/main/java/com/core/banking/repository/SavingAccountRepository.java
@@ -4,17 +4,12 @@ import com.core.banking.entity.SavingAccount;
 import com.core.banking.enums.SavingAccountStatus;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
-
-import jakarta.persistence.LockModeType;
 
 @Repository
 public interface SavingAccountRepository extends JpaRepository<SavingAccount, String> {
